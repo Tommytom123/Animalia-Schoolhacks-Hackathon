2abb3c941dadc654a0db7136cc02fcdfa49bf8cc
Tommytom123 <tommytom2006@icloud.com>
2022-06-04 15:39:58 +0200

    Changed File Structure

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-from flask import Flask, render_template, request, jsonify
-import pandas as pd
-
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():                              #Main Page
-    return render_template("Home.html")
-
-@app.route("/fetch", methods=["GET", "POST"])
-def fetch():                              #Request specific animal data
-    if request.method == "POST":
-        jsonData = request.get_json()
-        print(jsonData)
-        data = pd.read_csv("C:\\Users\\Tom Brouwers\\Documents\\Python\\Animalia\\Animalia-Schoolhacks-Hackathon\\database\\animal_locations.csv")
-        key = jsonData["key"]
-        return {
-            'species' : data["species"][key],
-            'type' : data["type"][key],
-            'extra_info' : data["info"][key]
-        }
-    return render_template("Home.html")
-
-@app.route("/all_data", methods=["GET", "POST"])
-def all_data():                              #Request specific animal data
-    if request.method == "POST":
-        data = pd.read_csv("C:\\Users\\Tom Brouwers\\Documents\\Python\\Animalia\\Animalia-Schoolhacks-Hackathon\\database\\animal_locations.csv")
-        data.drop("info", axis = 1, inplace = True)
-        data_json = data.to_json(None, indent = 1, orient = 'records')
-        print(data_json)
-        return data_json
-    return render_template("Home.html")
+from animalia import app
 
 if __name__ == "__main__":
     app.run(debug=True)
