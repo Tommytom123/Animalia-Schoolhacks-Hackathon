3fe932fe1f14cbb17dee54f0c1014a9fa0c7a901
Tommytom123 <tommytom2006@icloud.com>
2022-06-04 19:30:57 +0200

    Working on Submit animals

--- animalia/__init__.py
+++ animalia/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "0966db99-36b6-41c0-b0bd-be21894ceb60"
 
-from animalia import forms
 from animalia import routes
--- animalia/routes.py
+++ animalia/routes.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import csv
 from animalia import app
 
-@app.route("/")
-def home():                              #Main Page
+@app.route("/", methods=["GET", "POST"])
+def home():                              
+    if request.method == "POST":
+        # key,species,latitude,longitude,type,info
+        print("i got here wooohoo")
+        print(request.form["species"])
+        csv.writer(open("C:\\Users\\Tom Brouwers\\Documents\\Python\\Animalia\\Animalia-Schoolhacks-Hackathon\\animalia\\database\\animal_locations.csv", "a+")).writerow(
+            [
+                3,
+                request.form["species"],
+                request.form["lat"],
+                request.form["long"],
+                request.form["animal"],
+                request.form["description"]
+            ]
+        )
+        return render_template("Home.html")
     return render_template("Home.html")
 
 @app.route("/fetch", methods=["GET", "POST"])
@@ -26,6 +42,6 @@ def all_data():                              #Request specific animal data
         data = pd.read_csv("C:\\Users\\Tom Brouwers\\Documents\\Python\\Animalia\\Animalia-Schoolhacks-Hackathon\\animalia\\database\\animal_locations.csv")
         data.drop("info", axis = 1, inplace = True)
         data_json = data.to_json(None, indent = 1, orient = 'records')
-        print(data_json)
+        #print(data_json)
         return data_json
-    return render_template("Home.html")
+    return render_template("Home.html")      
