6e9c91b866010391a2001db14c0f60f04ec40238
Tommytom123 <tommytom2006@icloud.com>
2022-06-04 09:19:50 +0200

    Added Fetch

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():                                     #Main Page
+def home():                              #Main Page
+    return render_template("Home.html")
+
+@app.route("/fetch", methods=["GET", "POST"])
+def fetch():                              #Request specific animal data
+    if request.method == "POST":
+        jsonData = request.get_json()
+        print(jsonData)
+        data = pd.read_csv("C:\\Users\\Tom Brouwers\\Documents\\Python\\Animalia\\Animalia-Schoolhacks-Hackathon\\database\\animal_locations.csv")
+        key = data["key"]
+        print("KEYS", key)
+        animal = data["animal"][key-1]
+        print("ANIMAL", animal)
+        return {
+            'response' : animal
+        }
     return render_template("Home.html")
 
 if __name__ == "__main__":
